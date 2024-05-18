4caf4a7e6ff7ade6da79df85a3fd0310f83bfcf3
Lucas Ronald <lucas.silva829@sptch.school>
2024-05-18 00:52:23 -0300

    Aualizando site

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -2,16 +2,16 @@ var usuarioModel = require("../models/usuarioModel");
 // var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
+    var usuario = req.body.UsuarioServer;
     var senha = req.body.senhaServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    if (usuario == undefined) {
+        res.status(400).send("Seu usuario está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
 
-        usuarioModel.autenticar(email, senha)
+        usuarioModel.autenticar(usuario, senha)
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
@@ -24,9 +24,9 @@ function autenticar(req, res) {
                             // .then((resultadoAquarios) => {
                                 // if (resultadoAquarios.length > 0) {
                                     res.json({
-                                        id: resultadoAutenticar[0].id,
+                                        id: resultadoAutenticar[0].idUsuario,
                                         email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
+                                        usuario: resultadoAutenticar[0].NomeUsuario,
                                         senha: resultadoAutenticar[0].senha
                                         // aquarios: resultadoAquarios
                                     });
@@ -35,7 +35,7 @@ function autenticar(req, res) {
                             //     }
                             // })
                     } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
+                        res.status(403).send("Usuario e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                     }
