a9484aaf1c83b72a6bd9d06108d7bebdb0d0dc42
Lucas Ronald <lucas.silva829@sptch.school>
2024-05-31 20:24:34 -0300

    atualizações

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,14 +1,15 @@
 // sessão
 function validarSessao() {
+  // Função para validar a sessão e exibir o nome de usuário
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-
     var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
+      b_usuario.innerHTML = nome;
     } else {
-        window.location = "../login.html";
+      // Redirecionar para a página de login se não houver sessão válida
+      window.location = "../login.html";
     }
 }
 
